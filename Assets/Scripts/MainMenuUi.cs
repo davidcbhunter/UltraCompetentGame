ca05417086a3ea971d30f41fbdf7d9b7083ce524
ILaxaiI <Lukas.Fath02@gmail.com>
2020-06-19 12:58:55 +0200

    masterpull

---
+++ Assets/Scripts/MainMenuUi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuUi : MonoBehaviour
+{
+    public void QuitButton() => Application.Quit();
+    public void PlayButton() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   
+}
